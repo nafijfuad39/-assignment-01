3b961a7321675e2ba8b01a88f6234373deb7ccd8
Nafij Fuad <nafijfuad39>
2024-01-05 22:42:54 +0600

    4th commit

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@
             var age = document.getElementById('ageInput').value;
             var resultCard = document.getElementById('result-card');
             var resultText = document.getElementById('result-text');
+            var resultImage = document.getElementById('result-image');
 
             if (age >= 18) {
+                resultImage.src = './image/adult.png';
                 resultText.textContent = 'You are an adult.';
             } else {
+                resultImage.src = './image/child.png'; 
                 resultText.textContent = 'You are a child.';
             }
             resultCard.style.display = 'block';
